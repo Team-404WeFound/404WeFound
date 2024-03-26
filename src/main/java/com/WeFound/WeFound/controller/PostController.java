d8721ef4f7749abfa55c9fdede645c58286a0365
dragon0622 <iamdragon.dev@gmail.com>
2024-03-26 17:43:24 +0900

    feat: 게시판 생성 로직 구현

--- src/main/java/com/WeFound/WeFound/controller/PostController.java
+++ src/main/java/com/WeFound/WeFound/controller/PostController.java
@@ -1,7 +1,14 @@
 package com.WeFound.WeFound.controller;
 
+import com.WeFound.WeFound.dto.AddPostRequest;
+import com.WeFound.WeFound.dto.PostResponse;
+import com.WeFound.WeFound.entity.Post;
 import com.WeFound.WeFound.service.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PostController {
@@ -12,6 +19,11 @@ public class PostController {
     }
 
     //todo 게시판 생성
+    @PostMapping("/posts")
+    public ResponseEntity<PostResponse> addPost(@RequestParam AddPostRequest request){
+        Post post = postService.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(post.toResponse());
+    }
     //todo 게시판 조회
     //todo 게시판 수정
     //todo 게시판 삭제
