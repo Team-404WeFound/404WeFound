333f060542d4de02bc400ce70ce259dc69174a72
dragon0622 <iamdragon.dev@gmail.com>
2024-03-26 16:39:35 +0900

    feat: PostService 생성

---
+++ src/main/java/com/WeFound/WeFound/service/PostService.java
@@ -0,0 +1,13 @@
+package com.WeFound.WeFound.service;
+
+import com.WeFound.WeFound.repository.PostRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+    private final PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+}
