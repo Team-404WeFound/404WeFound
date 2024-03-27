7675f2e700671be5fb9e22ea60bf4838e8fe1200
dragon0622 <iamdragon.dev@gmail.com>
2024-03-27 09:52:16 +0900

    refactor: 컬럼명 변경
    테이블 컬럼명 변경으로 인한 엔티티 컬럼명 변경

--- src/main/java/com/WeFound/WeFound/entity/Question.java
+++ src/main/java/com/WeFound/WeFound/entity/Question.java
@@ -3,21 +3,25 @@ package com.WeFound.WeFound.entity;
 import com.WeFound.WeFound.dto.QuestionResponse;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Entity
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Question {
     @Id
-    @Column(name = "post_id", updatable = false)
-    private int post_id;
+    @Column(name = "question_id", updatable = false)
+    private int question_id;
 
     @Column(name = "user_id")
     private int user_id;
@@ -28,13 +32,13 @@ public class Question {
     @Column(name = "content")
     private String content;
 
-
-    @Column(name = "like_count")
-    private int like_count;
-
     @CreatedDate
-    @Column(name = "wtime")
-    private LocalDateTime create_time;
+    @Column(name = "create_at")
+    private LocalDateTime create_at;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updated_at;
 
     @Builder
     public Question(String title, String content){
