90cffad97fa9f2a2274bf87b8fbc18816fc1b291
dragon0622 <iamdragon.dev@gmail.com>
2024-03-27 12:04:16 +0900

    refactor: 누락 변수 추가

--- src/main/java/com/WeFound/WeFound/dto/AddQuestionRequest.java
+++ src/main/java/com/WeFound/WeFound/dto/AddQuestionRequest.java
@@ -5,11 +5,15 @@ import com.WeFound.WeFound.entity.Question;
 public class AddQuestionRequest {
     private String title;
     private String content;
+    private Long user_id;
+    private Long question_id;
 
     public Question toEntity(){
         return Question.builder()
                 .title(title)
                 .content(content)
+                .question_id(question_id)
+                .user_id(user_id)
                 .build();
     }
 }
