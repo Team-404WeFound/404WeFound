d6486b96890be95dd39d307b0f1b3a839d31c79f
dragon0622 <iamdragon.dev@gmail.com>
2024-03-27 09:53:52 +0900

    refactor: 변수명 변경
    엔티티 컬럼명 변경으로 인한 변수명 변경

--- src/main/java/com/WeFound/WeFound/dto/QuestionResponse.java
+++ src/main/java/com/WeFound/WeFound/dto/QuestionResponse.java
@@ -9,17 +9,15 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class QuestionResponse {
-    private int post_id;
+    private int question_id;
     private int user_id;
     private String title;
     private String content;
-    private int like_count;
 
     public QuestionResponse(Question question){
-        post_id = question.getPost_id();
+        question_id = question.getQuestion_id();
         user_id = question.getUser_id();
         title = question.getTitle();
         content = question.getContent();
-        like_count = question.getLike_count();
     }
 }
