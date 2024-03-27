52eebdd4ab5b818bdb6ba424dbe8862079d5238d
dragon0622 <iamdragon.dev@gmail.com>
2024-03-27 15:26:15 +0900

    feat: 게시판 조회 컨트롤러 구현

--- src/main/java/com/WeFound/WeFound/controller/QuestionController.java
+++ src/main/java/com/WeFound/WeFound/controller/QuestionController.java
@@ -6,10 +6,9 @@ import com.WeFound.WeFound.entity.Question;
 import com.WeFound.WeFound.service.QuestionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class QuestionController {
@@ -26,6 +25,23 @@ public class QuestionController {
         return ResponseEntity.status(HttpStatus.CREATED).body(question.toResponse());
     }
     //todo 게시판 조회
+    @GetMapping("/api/questions")
+    public ResponseEntity<List<QuestionResponse>> showQuestion(){
+        List<Question> questionList = questionService.findAll();
+        List<QuestionResponse> questionResponseList = questionList
+                .stream()
+                .map(QuestionResponse::new)
+                .toList();
+
+        return ResponseEntity.ok(questionResponseList);
+    }
+
+    //todo 게시판 단권 조회
+    @GetMapping("/api/questions/{question_id}")
+    public ResponseEntity<QuestionResponse> showOneQuestion(@PathVariable Long question_id){
+        Question question = questionService.findById(question_id);
+        return ResponseEntity.ok(question.toResponse());
+    }
     //todo 게시판 수정
     //todo 게시판 삭제
 
