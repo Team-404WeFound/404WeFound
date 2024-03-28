ef0c5460ec92a82444810009f1804c4e25cb3dc4
gyenug <vovogo03@gmail.com>
2024-03-28 15:39:49 +0900

    0328 ver3

---
+++ src/main/java/com/WeFound/WeFound/MemberEntity/MemberEntity.java
@@ -0,0 +1,44 @@
+package com.WeFound.WeFound.MemberEntity;
+
+import com.WeFound.WeFound.MemberDTO.MemberDTO;
+import lombok.Getter;
+import lombok.Setter;
+
+import jakarta.persistence.*;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "member_table")
+public class MemberEntity {
+    @Id // pk 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private Long id;
+
+    @Column(unique = true)
+    private String memberEmail;
+
+    @Column
+    private String memberPassword;
+
+    @Column
+    private String memberName;
+
+    public static MemberEntity toMemberEntity(MemberDTO memberDTO) {
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setMemberEmail(memberDTO.getMemberEmail());
+        memberEntity.setMemberPassword(memberDTO.getMemberPassword());
+        memberEntity.setMemberName(memberDTO.getMemberName());
+        return memberEntity;
+    }
+
+    public static MemberEntity toUpdateMemberEntity(MemberDTO memberDTO) {
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setId(memberDTO.getId());
+        memberEntity.setMemberEmail(memberDTO.getMemberEmail());
+        memberEntity.setMemberPassword(memberDTO.getMemberPassword());
+        memberEntity.setMemberName(memberDTO.getMemberName());
+        return memberEntity;
+    }
+
+}
--- src/main/java/com/WeFound/WeFound/repository/HelloRepository.java
+++
@@ -1,9 +0,0 @@
-package com.WeFound.WeFound.repository;
-
-import com.WeFound.WeFound.entity.Hello;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HelloRepository extends JpaRepository<Hello, Long> {
-}
