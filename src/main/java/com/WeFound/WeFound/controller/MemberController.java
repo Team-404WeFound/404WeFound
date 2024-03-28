a94fd0a65e23d8d3e6455e9114feb94080344c00
gyenug <vovogo03@gmail.com>
2024-03-28 10:46:14 +0900

    0328 ver1

--- src/main/java/com/WeFound/WeFound/controller/MemberController.java
+++ src/main/java/com/WeFound/WeFound/controller/MemberController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 
-import javax.servlet.http.HttpSession; //안됨
+import javax.servlet.http.HttpSession; ///안됨
 
 
 import java.util.List;
