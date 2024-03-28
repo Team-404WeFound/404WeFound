87a81146268497dada9966f4ad9b515c66fa60b7
gyenug <vovogo03@gmail.com>
2024-03-28 16:54:04 +0900

    0328 ver3.1

---
+++ src/test/java/com/WeFound/WeFound/Team404WeFoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WeFound.WeFound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team404WeFoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
