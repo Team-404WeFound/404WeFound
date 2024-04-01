acc28240b99cb2d9c7e36fd52e48970a2d1422ce
dragon0622 <iamdragon.dev@gmail.com>
2024-04-01 20:11:09 +0900

    feat: CommentService 로직 구현

--- src/main/java/com/WeFound/WeFound/service/CommentService.java
+++ src/main/java/com/WeFound/WeFound/service/CommentService.java
@@ -1,7 +1,28 @@
 package com.WeFound.WeFound.service;
 
+import com.WeFound.WeFound.dto.AddCommentRequest;
+import com.WeFound.WeFound.dto.CommentResponse;
+import com.WeFound.WeFound.entity.Comment;
+import com.WeFound.WeFound.entity.Question;
+import com.WeFound.WeFound.repository.CommentRepository;
+import com.WeFound.WeFound.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.query.sqm.internal.QuerySqmImpl;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentService {
+    private final CommentRepository commentRepository;
+    private final QuestionRepository questionRepository;
+
+    public CommentService(CommentRepository commentRepository, QuestionRepository questionRepository) {
+        this.commentRepository = commentRepository;
+        this.questionRepository = questionRepository;
+    }
+
+    public Comment save(Long question_id, AddCommentRequest request){
+        Question question = questionRepository.findById(question_id).orElseThrow(IllegalArgumentException::new);
+        Comment comment = request.toEntity(question);
+        return commentRepository.save(comment);
+    }
 }
