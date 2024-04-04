19b9d73cfe1436939d14581f03090ba20de7d95a
dragon0622 <iamdragon.dev@gmail.com>
2024-04-04 19:50:14 +0900

    feat: 게시글 수정 가능(미완)
    수정은 가능하나, 작성한 본인만 수정할 수 있게 유효성 체크가 추가적으로 필요합니다.

--- src/main/resources/static/scripts/question.js
+++ src/main/resources/static/scripts/question.js
@@ -17,3 +17,28 @@ if (createButton) {
         })
     })
 }
+
+
+const modifyButton = document.getElementById("modify-btn");
+
+if (modifyButton) {
+    // 클릭 이벤트가 감지되면 수정 API 요청
+    modifyButton.addEventListener('click', event => {
+        let params = new URLSearchParams(location.search);
+        let questionId = params.get('questionId');
+
+        fetch(`/api/questions/${questionId}`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: document.getElementById('title').value,
+                content: document.getElementById('content').value
+            })
+        }).then(() => {
+            alert('수정이 완료되었습니다');
+            location.replace(`/api/questions/${questionId}`);
+        });
+    });
+}
