7edf17c5837f0d8a04878e4c9b70a861346726eb
gyenug <vovogo03@gmail.com>
2024-04-05 03:15:14 +0900

    feat: Like like컨트롤러와 계속되는 오류

--- src/main/java/com/WeFound/WeFound/repository/LikeRepository.java
+++ src/main/java/com/WeFound/WeFound/repository/LikeRepository.java
@@ -5,16 +5,21 @@
 package com.WeFound.WeFound.repository;
 
 import com.WeFound.WeFound.entity.Like;
+import com.WeFound.WeFound.service.LikeService;
 import com.WeFound.WeFound.entity.User;
 import com.WeFound.WeFound.entity.Question;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 import java.util.List;
 
-/*
+
+@Repository
+public interface LikeRepository extends JpaRepository<Like, Long> {
+    void deleteByUserIdAndQuestionId(Long user_Id, Long question_Id);
+
+   /* Like findByUserIdAndQuestionId(Long user_Id, Long question_Id);*/
+}/*
     @Repository
     public interface LikeRepository extends JpaRepository<Like, Long> {
 
@@ -25,10 +30,3 @@ import java.util.List;
 
 */
 
-
-@Repository
-public interface LikeRepository extends JpaRepository<Like, Long> {
-    void deleteByUserIdAndQuestionId(Long user_id, Long question_id);
-
-   /* Like findByUserIdAndQuestionId(Long userid, Long questionid);*/
-}
