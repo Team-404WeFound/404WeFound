cc1d98de514664ea7fad01718b1803e223b4938e
dragon0622 <iamdragon.dev@gmail.com>
2024-04-04 10:24:17 +0900

    feat: inputQuestion.html 으로 이동 로직

--- src/main/java/com/WeFound/WeFound/controller/QuestionsPageController.java
+++ src/main/java/com/WeFound/WeFound/controller/QuestionsPageController.java
@@ -1,10 +1,13 @@
 package com.WeFound.WeFound.controller;
 
 import com.WeFound.WeFound.dto.QuestionViewResponse;
+import com.WeFound.WeFound.entity.Question;
 import com.WeFound.WeFound.service.QuestionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -24,5 +27,17 @@ public class QuestionsPageController {
         model.addAttribute("questions", questions);   // model에 블로그 글 리스트 저장
 
         return "main";   // articleList.html라는 뷰 조회
+    }
+
+    @GetMapping("/inputQuestion")
+    public String newQuestion(@RequestParam(required = false) Long questionId, Model model){
+        if (questionId == null){
+            model.addAttribute("question", new QuestionViewResponse());
+        }
+        else{
+            Question question = questionService.findById(questionId);
+            model.addAttribute("question", new QuestionViewResponse(question));
+        }
+        return "inputQuestion";
     }
 }
