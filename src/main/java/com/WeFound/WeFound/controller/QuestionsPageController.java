a3d90f7e4b1259db92fb139c8333f4950f303a2a
dragon0622 <iamdragon.dev@gmail.com>
2024-04-04 13:32:18 +0900

    feat: 뷰 전환 컨트롤러 구현

--- src/main/java/com/WeFound/WeFound/controller/QuestionsPageController.java
+++ src/main/java/com/WeFound/WeFound/controller/QuestionsPageController.java
@@ -6,6 +6,7 @@ import com.WeFound.WeFound.service.QuestionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -20,13 +21,21 @@ public class QuestionsPageController {
     }
 
     @GetMapping("/api/questions")
-    public String getArticles(Model model) {
+    public String getQuestions(Model model) {
         List<QuestionViewResponse> questions = questionService.findAll().stream()
                 .map(QuestionViewResponse::new)
                 .toList();
         model.addAttribute("questions", questions);   // model에 블로그 글 리스트 저장
 
-        return "main";   // articleList.html라는 뷰 조회
+        return "main";   // main.html라는 뷰 조회
+    }
+
+    @GetMapping("/api/questions/{questionId}")   //상세 html 뷰로 전환을 위한 Get
+    public String getQuestion(@PathVariable Long questionId, Model model) {
+        Question question = questionService.findById(questionId);
+        model.addAttribute("question", new QuestionViewResponse(question));
+
+        return "questionDetail";
     }
 
     @GetMapping("/inputQuestion")
