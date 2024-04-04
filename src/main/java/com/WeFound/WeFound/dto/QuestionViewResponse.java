e993de27ca1deca3c922f14f38507edcebe37ae3
dragon0622 <iamdragon.dev@gmail.com>
2024-04-04 10:24:31 +0900

    feat: 기본 생성자 추가

--- src/main/java/com/WeFound/WeFound/dto/QuestionViewResponse.java
+++ src/main/java/com/WeFound/WeFound/dto/QuestionViewResponse.java
@@ -2,8 +2,10 @@ package com.WeFound.WeFound.dto;
 
 import com.WeFound.WeFound.entity.Question;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class QuestionViewResponse {
     private Long questionId;
     private String title;
