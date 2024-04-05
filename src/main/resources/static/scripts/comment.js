473f7f8c96cc838a95c7f941bdef09c1fba9037e
dragon0622 <iamdragon.dev@gmail.com>
2024-04-05 16:26:18 +0900

    feat: 변수 추가
    questionId 를 찾을 변수 params, questionId 추가

--- src/main/resources/static/scripts/comment.js
+++ src/main/resources/static/scripts/comment.js
@@ -2,18 +2,20 @@ const createCommentButton = document.getElementById('create-comment-btn');
 
 if (createCommentButton) {
     createCommentButton.addEventListener('click', event => {
-        fetch(`/api/questions`, {
+        let params = new URLSearchParams(location.search);
+        let questionId = params.get('questionId');
+
+        fetch(`/api/questions/{questionId}`, {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
             },
             body : JSON.stringify({
-                title: document.getElementById('title').value,
                 content: document.getElementById('content').value
             }),
         }).then(() => {
             alert('등록 완료되었습니다');
-            location.replace("/api/questions");
+            location.replace(`/api/questions/${questionId}`);
         })
     })
 }
