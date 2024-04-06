7c86f0321260880fdbb65d4e8ec39b7b15904c1f
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-04-06 22:40:23 +0900

    feat: answer기능 구현

--- src/main/java/com/WeFound/WeFound/repository/AnswerRepository.java
+++ src/main/java/com/WeFound/WeFound/repository/AnswerRepository.java
@@ -1,12 +1,2 @@
-package com.WeFound.WeFound.repository;
-
-import com.WeFound.WeFound.entity.Answer;
-import com.WeFound.WeFound.entity.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface AnswerRepository extends JpaRepository<Answer,Long> {
-    List<Answer> findByQuestion(Question question);
-
+package com.WeFound.WeFound.repository;public interface AnswerRepository {
 }
